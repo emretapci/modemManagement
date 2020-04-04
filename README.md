d8537a3ef0b99eab4da47f055529e135fe4decec
Emre Tapci <emre.tapci@gmail.com>
2020-04-04 23:20:03 +0300

    initial

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # modemManagement
 backend API for HUAWEI HG255s modem management
+manages device LAN access.
+
+## GET /devices
+returns all devices registered on the modem
+
+## GET /filters
+returns all MAC filters registered on the modem
+
+## PUT /filters
+sets a MAC filter on the modem  
+body:  
+```
+{
+	mac: [
+		<mac address> *
+	],
+	days: {
+		mon: '8:00-10:00', //allowed time interval for monday
+		tue: '10:00-11:00',
+		thu: '10:00-11:00'
+	}
+	OR
+	days: '8:00-10:00' //allowed time interval for all days of the week
+}
+```
+
+## PUT /name
+name an existing device  
+body:  
+```
+{
+	mac: <mac>,
+	name: <name>
+}
+```
+
+## PUT /enable
+enable a device  
+body:  
+```
+{
+	mac: <mac address>
+	OR
+	name: <name>
+}
+```
+
+## PUT /enable
+disable a device  
+body:  
+```
+{
+	mac: <mac address>
+	OR
+	name: <name>
+}
+```
+
---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const modem = require('./modem');
+
+const app = express();
+
+app.use(cors({ credentials: true, origin: ['http://localhost:3000'] }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/json', limit: '500kb' }));
+
+app.get('/devices', async (req, res) => {
+	res.status(200).json(await modem.getDevices());
+});
+
+app.get('/filters', async (req, res) => {
+	res.status(200).json(await modem.getMacFilters());
+});
+
+app.put('/filters', async (req, res) => {
+	res.status(200).json(await modem.setMacFilters(req.body));
+});
+
+/*
+{
+	mac: <mac>,
+	name: <name>
+}
+*/
+app.put('/name', (req, res) => {
+	res.status(200).json(modem.setDeviceName(req.body.mac, req.body.name));
+});
+
+app.put('/enable', async (req, res) => {
+	res.status(200).json(await modem.enableDevice(req.body.mac));
+});
+
+app.put('/disable', async (req, res) => {
+	res.status(200).json(await modem.disableDevice(req.body.mac));
+});
+
+app.use((err, req, res) => {
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+	res.status(err.status || 500);
+	res.render('error');
+});
+
+const start = () => app.listen(process.env.PORT ? process.env.PORT : 3001);
+
+module.exports = { start };
