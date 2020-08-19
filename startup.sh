16d842ec84c9c1806dc3802d3571bf0b3ebbb5ad
Emre Tapci <emre.tapci@gmail.com>
2020-08-20 00:12:13 +0300

    .

--- startup.sh
+++ startup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-git clone https://github.com/emretapci/modemManagement
-cd modemManagement
+if [ -e modemManagement ]
+then cd modemManagement && git pull
+else git clone https://github.com/emretapci/modemManagement && cd modemManagement
+fi
 npm i
 cd frontend
 npm i
